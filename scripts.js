1db0766717b7fd5b54aa71e8ce959fd37ebfde27
Filipas Egor <apple@MacBook-Air-apple.local>
2019-07-18 19:22:07 +0300

    New

---
+++ scripts.js
@@ -0,0 +1,55 @@
+ScrollReveal().reveal('.is-philosophy', {
+  delay: 600,
+  duration: 200,
+  distance: '150%',
+  origin: 'bottom',
+  opacity: 0.6,
+  rotate: {
+    x: 20,
+    z: 20
+  }
+});
+ScrollReveal().reveal('.is-call-and-write', {
+  delay: 500,
+  duration: 200,
+  distance: '150%',
+  origin: 'bottom',
+  opacity: 0.6,
+  rotate: {
+    x: 20,
+    z: 20
+  }
+});
+ScrollReveal().reveal('.me', {
+  delay: 500,
+  duration: 200,
+  distance: '150%',
+  origin: 'bottom',
+  opacity: 0.6,
+  rotate: {
+    x: 20,
+    z: 20
+  }
+});
+ScrollReveal().reveal('.capabilities', {
+  delay: 500,
+  duration: 200,
+  distance: '150%',
+  origin: 'bottom',
+  opacity: 0.6,
+  rotate: {
+    x: 20,
+    z: 20
+  }
+});
+ScrollReveal().reveal('.main-paragraph', {
+  delay: 500,
+  duration: 200,
+  distance: '150%',
+  origin: 'bottom',
+  opacity: 0.6,
+  rotate: {
+    x: 20,
+    z: 20
+  }
+});
